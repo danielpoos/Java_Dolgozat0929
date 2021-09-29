9e32a69543b6a646d335d4177086e86928dda174
Poós Dániel <poosdani@gmail.com>
2021-09-29 15:14:47 +0200

    adding a bejegyzes function to the main

--- src/hu/petrik/Main.java
+++ src/hu/petrik/Main.java
@@ -1,7 +1,26 @@
 package hu.petrik;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    static Scanner s = new Scanner(System.in);
+    static List<Bejegyzes> bejegyList = new ArrayList<>();
+
+    public static void bejegyzes(){
+        System.out.println("Hány darab bejegyzést szeretne hozzáadni?");
+        int db =s.nextInt();
+        for (int i = 0; i < db; i++) {
+            System.out.println("Adja meg a szerző nevét: ");
+            String szer = s.next();
+            System.out.println("Adja meg a tartalmát: ");
+            String tart = s.next();
+            bejegyList.add(new Bejegyzes(szer,tart));
+        }
+    }
 
     public static void main(String[] args) {
+        bejegyzes();
     }
 }
